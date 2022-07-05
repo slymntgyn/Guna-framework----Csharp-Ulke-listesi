96595984072accc737dcc854cdb23da50922173c
slymntgyn <ins.1907@gmail.com>
2022-07-05 19:36:28 +0300

    Gunaframwork -- Csharp -- ulkelistesi
    Afrika, Asya, Kuzey Amerika, Güney Amerika, Okyanusya, Avrupa ve adalarda bulunan ulkelerin listesini API kullanarak kullanciya gosteren desktop uygulamasidir. Uygulamani grafiginin daha güzel olmasi icin guna framework kullanilmistir.

---
+++ Ülkelerlisteleguna/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿
+namespace Ülkelerlisteleguna
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.asya = new Guna.UI.WinForms.GunaButton();
+            this.afrika = new Guna.UI.WinForms.GunaButton();
+            this.kuzeyamerika = new Guna.UI.WinForms.GunaButton();
+            this.güneyamerika = new Guna.UI.WinForms.GunaButton();
+            this.avrupa = new Guna.UI.WinForms.GunaButton();
+            this.okyanusya = new Guna.UI.WinForms.GunaButton();
+            this.adalar = new Guna.UI.WinForms.GunaButton();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.gunaPictureBox1 = new Guna.UI.WinForms.GunaPictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.gunaPictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // asya
+            // 
+            this.asya.AnimationHoverSpeed = 1F;
+            this.asya.AnimationSpeed = 0.03F;
+            this.asya.BackColor = System.Drawing.Color.Transparent;
+            this.asya.BaseColor = System.Drawing.Color.Olive;
+            this.asya.BorderColor = System.Drawing.Color.Black;
+            this.asya.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.asya.FocusedColor = System.Drawing.Color.Red;
+            this.asya.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.asya.ForeColor = System.Drawing.Color.White;
+            this.asya.Image = ((System.Drawing.Image)(resources.GetObject("asya.Image")));
+            this.asya.ImageSize = new System.Drawing.Size(20, 20);
+            this.asya.Location = new System.Drawing.Point(28, 27);
+            this.asya.Name = "asya";
+            this.asya.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
+            this.asya.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.asya.OnHoverForeColor = System.Drawing.Color.White;
+            this.asya.OnHoverImage = null;
+            this.asya.OnPressedColor = System.Drawing.Color.Black;
+            this.asya.Radius = 15;
+            this.asya.Size = new System.Drawing.Size(160, 42);
+            this.asya.TabIndex = 0;
+            this.asya.Text = "Asya kıtasını listele";
+            this.asya.Click += new System.EventHandler(this.asya_Click);
+            // 
+            // afrika
+            // 
+            this.afrika.AnimationHoverSpeed = 1F;
+            this.afrika.AnimationSpeed = 0.03F;
+            this.afrika.BackColor = System.Drawing.Color.Transparent;
+            this.afrika.BaseColor = System.Drawing.Color.Olive;
+            this.afrika.BorderColor = System.Drawing.Color.Black;
+            this.afrika.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.afrika.FocusedColor = System.Drawing.Color.Red;
+            this.afrika.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.afrika.ForeColor = System.Drawing.Color.White;
+            this.afrika.Image = ((System.Drawing.Image)(resources.GetObject("afrika.Image")));
+            this.afrika.ImageSize = new System.Drawing.Size(20, 20);
+            this.afrika.Location = new System.Drawing.Point(28, 99);
+            this.afrika.Name = "afrika";
+            this.afrika.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
+            this.afrika.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.afrika.OnHoverForeColor = System.Drawing.Color.White;
+            this.afrika.OnHoverImage = null;
+            this.afrika.OnPressedColor = System.Drawing.Color.Black;
+            this.afrika.Radius = 15;
+            this.afrika.Size = new System.Drawing.Size(160, 42);
+            this.afrika.TabIndex = 1;
+            this.afrika.Text = "Afrika kıtasını listele";
+            this.afrika.Click += new System.EventHandler(this.afrika_Click);
+            // 
+            // kuzeyamerika
+            // 
+            this.kuzeyamerika.AnimationHoverSpeed = 1F;
+            this.kuzeyamerika.AnimationSpeed = 0.03F;
+            this.kuzeyamerika.BackColor = System.Drawing.Color.Transparent;
+            this.kuzeyamerika.BaseColor = System.Drawing.Color.Olive;
+            this.kuzeyamerika.BorderColor = System.Drawing.Color.Black;
+            this.kuzeyamerika.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.kuzeyamerika.FocusedColor = System.Drawing.Color.Red;
+            this.kuzeyamerika.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.kuzeyamerika.ForeColor = System.Drawing.Color.White;
+            this.kuzeyamerika.Image = ((System.Drawing.Image)(resources.GetObject("kuzeyamerika.Image")));
+            this.kuzeyamerika.ImageSize = new System.Drawing.Size(20, 20);
+            this.kuzeyamerika.Location = new System.Drawing.Point(28, 163);
+            this.kuzeyamerika.Name = "kuzeyamerika";
+            this.kuzeyamerika.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
+            this.kuzeyamerika.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.kuzeyamerika.OnHoverForeColor = System.Drawing.Color.White;
+            this.kuzeyamerika.OnHoverImage = null;
+            this.kuzeyamerika.OnPressedColor = System.Drawing.Color.Black;
+            this.kuzeyamerika.Radius = 15;
+            this.kuzeyamerika.Size = new System.Drawing.Size(160, 42);
+            this.kuzeyamerika.TabIndex = 2;
+            this.kuzeyamerika.Text = "Kuzey Amerkayı listele";
+            this.kuzeyamerika.Click += new System.EventHandler(this.kuzeyamerika_Click);
+            // 
+            // güneyamerika
+            // 
+            this.güneyamerika.AnimationHoverSpeed = 1F;
+            this.güneyamerika.AnimationSpeed = 0.03F;
+            this.güneyamerika.BackColor = System.Drawing.Color.Transparent;
+            this.güneyamerika.BaseColor = System.Drawing.Color.Olive;
+            this.güneyamerika.BorderColor = System.Drawing.Color.Black;
+            this.güneyamerika.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.güneyamerika.FocusedColor = System.Drawing.Color.Red;
+            this.güneyamerika.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.güneyamerika.ForeColor = System.Drawing.Color.White;
+            this.güneyamerika.Image = ((System.Drawing.Image)(resources.GetObject("güneyamerika.Image")));
+            this.güneyamerika.ImageSize = new System.Drawing.Size(20, 20);
+            this.güneyamerika.Location = new System.Drawing.Point(28, 220);
+            this.güneyamerika.Name = "güneyamerika";
+            this.güneyamerika.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
+            this.güneyamerika.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.güneyamerika.OnHoverForeColor = System.Drawing.Color.White;
+            this.güneyamerika.OnHoverImage = null;
+            this.güneyamerika.OnPressedColor = System.Drawing.Color.Black;
+            this.güneyamerika.Radius = 15;
+            this.güneyamerika.Size = new System.Drawing.Size(160, 42);
+            this.güneyamerika.TabIndex = 3;
+            this.güneyamerika.Text = "Güney Amerkayı listele";
+            this.güneyamerika.Click += new System.EventHandler(this.güneyamerika_Click);
+            // 
+            // avrupa
+            // 
+            this.avrupa.AnimationHoverSpeed = 1F;
+            this.avrupa.AnimationSpeed = 0.03F;
+            this.avrupa.BackColor = System.Drawing.Color.Transparent;
+            this.avrupa.BaseColor = System.Drawing.Color.Olive;
+            this.avrupa.BorderColor = System.Drawing.Color.Black;
+            this.avrupa.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.avrupa.FocusedColor = System.Drawing.Color.Red;
+            this.avrupa.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.avrupa.ForeColor = System.Drawing.Color.White;
+            this.avrupa.Image = ((System.Drawing.Image)(resources.GetObject("avrupa.Image")));
+            this.avrupa.ImageSize = new System.Drawing.Size(20, 20);
+            this.avrupa.Location = new System.Drawing.Point(28, 332);
+            this.avrupa.Name = "avrupa";
+            this.avrupa.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
+            this.avrupa.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.avrupa.OnHoverForeColor = System.Drawing.Color.White;
+            this.avrupa.OnHoverImage = null;
+            this.avrupa.OnPressedColor = System.Drawing.Color.Black;
+            this.avrupa.Radius = 15;
+            this.avrupa.Size = new System.Drawing.Size(160, 42);
+            this.avrupa.TabIndex = 4;
+            this.avrupa.Text = "Avrupa kıtasını listele";
+            this.avrupa.Click += new System.EventHandler(this.avrupa_Click);
+            // 
+            // okyanusya
+            // 
+            this.okyanusya.AnimationHoverSpeed = 1F;
+            this.okyanusya.AnimationSpeed = 0.03F;
+            this.okyanusya.BackColor = System.Drawing.Color.Transparent;
+            this.okyanusya.BaseColor = System.Drawing.Color.Olive;
+            this.okyanusya.BorderColor = System.Drawing.Color.Black;
+            this.okyanusya.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.okyanusya.FocusedColor = System.Drawing.Color.Red;
+            this.okyanusya.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.okyanusya.ForeColor = System.Drawing.Color.White;
+            this.okyanusya.Image = ((System.Drawing.Image)(resources.GetObject("okyanusya.Image")));
+            this.okyanusya.ImageSize = new System.Drawing.Size(20, 20);
+            this.okyanusya.Location = new System.Drawing.Point(28, 284);
+            this.okyanusya.Name = "okyanusya";
+            this.okyanusya.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
+            this.okyanusya.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.okyanusya.OnHoverForeColor = System.Drawing.Color.White;
+            this.okyanusya.OnHoverImage = null;
+            this.okyanusya.OnPressedColor = System.Drawing.Color.Black;
+            this.okyanusya.Radius = 15;
+            this.okyanusya.Size = new System.Drawing.Size(160, 42);
+            this.okyanusya.TabIndex = 5;
+            this.okyanusya.Text = "Okyanusya kıtasını listele";
+            this.okyanusya.Click += new System.EventHandler(this.okyanusya_Click);
+            // 
+            // adalar
+            // 
+            this.adalar.AnimationHoverSpeed = 1F;
+            this.adalar.AnimationSpeed = 0.03F;
+            this.adalar.BackColor = System.Drawing.Color.Transparent;
+            this.adalar.BaseColor = System.Drawing.Color.Olive;
+            this.adalar.BorderColor = System.Drawing.Color.Black;
+            this.adalar.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.adalar.FocusedColor = System.Drawing.Color.Red;
+            this.adalar.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.adalar.ForeColor = System.Drawing.Color.White;
+            this.adalar.Image = ((System.Drawing.Image)(resources.GetObject("adalar.Image")));
+            this.adalar.ImageSize = new System.Drawing.Size(20, 20);
+            this.adalar.Location = new System.Drawing.Point(28, 396);
+            this.adalar.Name = "adalar";
+            this.adalar.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
+            this.adalar.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.adalar.OnHoverForeColor = System.Drawing.Color.White;
+            this.adalar.OnHoverImage = null;
+            this.adalar.OnPressedColor = System.Drawing.Color.Black;
+            this.adalar.Radius = 15;
+            this.adalar.Size = new System.Drawing.Size(160, 42);
+            this.adalar.TabIndex = 6;
+            this.adalar.Text = "Adaları listele";
+            this.adalar.Click += new System.EventHandler(this.adalar_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.SandyBrown;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(248, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(501, 407);
+            this.listBox1.TabIndex = 7;
+            // 
+            // gunaPictureBox1
+            // 
+            this.gunaPictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.gunaPictureBox1.BaseColor = System.Drawing.Color.White;
+            this.gunaPictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("gunaPictureBox1.ErrorImage")));
+            this.gunaPictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("gunaPictureBox1.Image")));
+            this.gunaPictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("gunaPictureBox1.InitialImage")));
+            this.gunaPictureBox1.Location = new System.Drawing.Point(755, 2);
+            this.gunaPictureBox1.Name = "gunaPictureBox1";
+            this.gunaPictureBox1.Size = new System.Drawing.Size(45, 37);
+            this.gunaPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.gunaPictureBox1.TabIndex = 8;
+            this.gunaPictureBox1.TabStop = false;
+            this.gunaPictureBox1.Click += new System.EventHandler(this.gunaPictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SandyBrown;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gunaPictureBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.adalar);
+            this.Controls.Add(this.okyanusya);
+            this.Controls.Add(this.avrupa);
+            this.Controls.Add(this.güneyamerika);
+            this.Controls.Add(this.kuzeyamerika);
+            this.Controls.Add(this.afrika);
+            this.Controls.Add(this.asya);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gunaPictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI.WinForms.GunaButton asya;
+        private Guna.UI.WinForms.GunaButton afrika;
+        private Guna.UI.WinForms.GunaButton kuzeyamerika;
+        private Guna.UI.WinForms.GunaButton güneyamerika;
+        private Guna.UI.WinForms.GunaButton avrupa;
+        private Guna.UI.WinForms.GunaButton okyanusya;
+        private Guna.UI.WinForms.GunaButton adalar;
+        private System.Windows.Forms.ListBox listBox1;
+        private Guna.UI.WinForms.GunaPictureBox gunaPictureBox1;
+    }
+}
+
---
+++ Ülkelerlisteleguna/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Xml;
+namespace Ülkelerlisteleguna
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string connection = "https://api.livescorexml.com/?user=demo&password=123456&get=areas";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void asya_Click(object sender, EventArgs e)
+        {
+            XDocument ülkeler = XDocument.Load(connection);
+            listBox1.Items.Clear();
+            for (int i = 2; i < 50; i++)
+            {
+                var ülkeadiadi = ülkeler.Descendants("area").ElementAt(i).Attribute("name").Value;
+                var kodu = ülkeler.Descendants("area").ElementAt(i).Attribute("countrycode").Value;
+                listBox1.Items.Add(ülkeadiadi + " Asya kıtasında bulunmaktadır kodu : " + kodu);
+            }
+        }
+
+        private void afrika_Click(object sender, EventArgs e)
+        {
+            XDocument ülkeler = XDocument.Load(connection);
+            listBox1.Items.Clear();
+            for (int i = 51; i < 110; i++)
+            {
+                var ülkeadiadi = ülkeler.Descendants("area").ElementAt(i).Attribute("name").Value;
+                var kodu = ülkeler.Descendants("area").ElementAt(i).Attribute("countrycode").Value;
+                listBox1.Items.Add(ülkeadiadi + " afrika kıtasında bulunmaktadır kodu : " + kodu);
+            }
+        }
+
+        private void kuzeyamerika_Click(object sender, EventArgs e)
+        {
+            XDocument ülkeler = XDocument.Load(connection);
+            listBox1.Items.Clear();
+            for (int i = 111; i < 156; i++)
+            {
+                var ülkeadiadi = ülkeler.Descendants("area").ElementAt(i).Attribute("name").Value;
+                var kodu = ülkeler.Descendants("area").ElementAt(i).Attribute("countrycode").Value;
+                listBox1.Items.Add(ülkeadiadi + " kuzey Amerikda bulunmaktadır kodu : " + kodu);
+            }
+        }
+
+        private void güneyamerika_Click(object sender, EventArgs e)
+        {
+            XDocument ülkeler = XDocument.Load(connection);
+            listBox1.Items.Clear();
+            for (int i = 157; i < 169; i++)
+            {
+                var ülkeadiadi = ülkeler.Descendants("area").ElementAt(i).Attribute("name").Value;
+                var kodu = ülkeler.Descendants("area").ElementAt(i).Attribute("countrycode").Value;
+                listBox1.Items.Add(ülkeadiadi + " Güney Amerikda bulunmaktadır kodu : " + kodu);
+            }
+        }
+
+        private void okyanusya_Click(object sender, EventArgs e)
+        {
+
+            XDocument ülkeler = XDocument.Load(connection);
+            listBox1.Items.Clear();
+            for (int i = 170; i < 191; i++)
+            {
+                var ülkeadiadi = ülkeler.Descendants("area").ElementAt(i).Attribute("name").Value;
+                var kodu = ülkeler.Descendants("area").ElementAt(i).Attribute("countrycode").Value;
+                listBox1.Items.Add(ülkeadiadi + " Okyanusya'da bulunmaktadır kodu : " + kodu);
+            }
+        }
+
+        private void avrupa_Click(object sender, EventArgs e)
+        {
+            XDocument ülkeler = XDocument.Load(connection);
+            listBox1.Items.Clear();
+            for (int i = 192; i < 260; i++)
+            {
+                var ülkeadiadi = ülkeler.Descendants("area").ElementAt(i).Attribute("name").Value;
+                var kodu = ülkeler.Descendants("area").ElementAt(i).Attribute("countrycode").Value;
+                listBox1.Items.Add(ülkeadiadi + " Avrupa'da bulunmaktadır kodu : " + kodu);
+            }
+        }
+
+        private void adalar_Click(object sender, EventArgs e)
+        {
+            XDocument ülkeler = XDocument.Load(connection);
+            listBox1.Items.Clear();
+            for (int i = 260; i < 265; i++)
+            {
+                var ülkeadiadi = ülkeler.Descendants("area").ElementAt(i).Attribute("name").Value;
+                var kodu = ülkeler.Descendants("area").ElementAt(i).Attribute("countrycode").Value;
+                listBox1.Items.Add(ülkeadiadi + " Bir ada'dır kodu : " + kodu);
+            }
+        }
+
+        private void gunaPictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
